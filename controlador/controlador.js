d520237b0b5969ef4469c13179f91dd7c1994d20
david roa <davidroa8@hotmail.com>
2021-11-05 20:28:59 -0500

    API LOGIN

--- controlador/controlador.js
+++ controlador/controlador.js
@@ -14,6 +14,22 @@ controlador.listItems = (req, res) => {
     })
 }
 
+controlador.listItemsWithId = (req, res) => {
+    const { id } = req.params;
+    let url = (req.originalUrl).slice(1);
+    url = url.replace(/\d/g, "").slice(0, -1);
+
+    req.getConnection((err, conn) => {
+        conn.query(`SELECT * FROM ${url} WHERE usuId = ?`, [id], (err, results) => {
+            if (err) {
+                res.json(err);
+            } else {
+                res.json(results);
+            }
+        })
+    })
+}
+
 controlador.createItems = (req, res) => {
     const url = (req.originalUrl).slice(1);
 
@@ -49,6 +65,33 @@ controlador.deleteItems = (req, res) => {
         conn.query(`DELETE FROM ${url} WHERE ${url.slice(0, 3)}Id = ?`, [id], (err, results) => {
             if (err) {
                 res.json(err);
+            }
+        })
+    })
+}
+
+controlador.loginHandler = (req, res) => {
+    const data = req.body;
+
+    req.getConnection((err, conn) => {
+        conn.query("SELECT * FROM usuarios WHERE usuNombre = ?", [data.usuNombre], (err, results) => {
+            if (err) {
+                res.json({
+                    message: 'Los datos introducidos no existen',
+                    type: 'danger'
+                })
+            } else if (data.usuContraseña === results[0].usuContraseña) {
+                res.json({
+                    usuId: results[0].usuId,
+                    usuNombre: results[0].usuNombre,
+                    usuCorreo: results[0].usuCorreo,
+                    usuRol: results[0].usuRol
+                })
+            } else {
+                res.json({
+                    message: 'Los datos introducidos son incorrectos',
+                    type: 'danger'
+                })
             }
         })
     })
--- rutas/rutas.js
+++ rutas/rutas.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const controlador = require('../controlador/controlador');
-const tables = ['/usuarios', '/testimonios', '/productos', '/servicios']
+const tables = ['/usuarios', '/testimonios', '/productos', '/servicios', '/compras', '/reservas']
 
 for (let item of tables){
     router.get(item, controlador.listItems);
+    router.get(`${item}/:id`, controlador.listItemsWithId);
     router.post(item, controlador.createItems);
     router.put(`${item}/:id`, controlador.updateItems);
     router.delete(`${item}/:id`, controlador.deleteItems);
 }
+
+router.post('/login', controlador.loginHandler);
 
 module.exports = router;
